15c6cbafc84dd1177f6389bb58dcba7ab5bbe869
aki <a.test.acount2020jp@gmail.com>
2020-10-10 22:24:41 +0900

    Gitにファイル登録

---
+++ switch.js
@@ -0,0 +1,16 @@
+//switch
+
+const i='hoge';
+
+switch(i){
+  case 'hage':
+    console.log('hageです');
+    break;
+  case 'hoge':
+    console.log('hogeです');
+    break;
+  default:
+    console.log('一致しません');
+    break;
+}
+
---
+++ test.js
@@ -0,0 +1,59 @@
+console.log('テスト');
+
+123 //数字
+"" //or 
+'' //どちらかで文字列
+
+
+/*
+コメントアウト複数行の場合
+*/
+
+const member ={
+    'name':'honda',
+    'height':'170',
+    'hobby':'succer'
+};
+
+console.log(member.hobby);
+
+const member_2 = {
+    'honda':{
+        'height':170,
+        'hobby':'succer'
+        
+    },
+    'kagawa':{
+        'height':180,
+        'hobby':'succer'
+    }
+};
+
+console.log(member_2.kagawa.height);
+
+const member_3={
+    '1kumi':{
+        'honda':{
+            'height':170,
+            'hobby':'succer'
+        },
+        'kagawa':{
+            'height':180,
+            'hobby':'succer'
+        }
+    },
+    '2kumi':{
+        'yashiro':{
+            'height':189,
+            'hobby':'baseball'
+        },
+        'takahashi':{
+            'height':179,
+            'hobby':'baseball'
+        }
+    }
+
+    };
+
+    console.log(member_3);
+
